8b835b75c61c65d29422cf82f0c44ba93118d49d
BatterySmooth <matt-booth@live.co.uk>
2026-02-26 18:14:18 +0000

    Enable mobile static header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css';
-// import { useState, useEffect, useRef } from "react"
 import Countdown from './components/Countdown';
 import ParallaxHeader from './components/ParallaxHeader';
 import Article from './components/Article';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  function useShouldDisableParallax() {
+    const [disable, setDisable] = useState(false);
+    useEffect(() => {
+      const touch = window.matchMedia("(pointer: coarse)").matches;
+      const noHover = window.matchMedia("(hover: none)").matches;
+      const reducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+      setDisable(touch || noHover || reducedMotion);
+    }, []);
+    return disable;
+  }
+
+  const disableParallax = useShouldDisableParallax();
+
   return (
     <>
-      <ParallaxHeader>
+      <ParallaxHeader disableParallax={disableParallax}>
         <Article>
           <></>
         </Article>
--- src/components/ParallaxHeader.tsx
+++ src/components/ParallaxHeader.tsx
@@ -1,26 +1,30 @@
 import { useEffect, useRef, type ReactNode } from "react"
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all'
 
+import full from "@/assets/layers/Full.png"
 import layer1 from "@/assets/layers/1.png"
 import layer2 from "@/assets/layers/2.png"
+import fire from "@/assets/layers/fire.gif"
 import layer3 from "@/assets/layers/3.png"
 import layer4 from "@/assets/layers/4.png"
 import layer5 from "@/assets/layers/5.png"
-import layer6 from "@/assets/layers/6.png"
+import layer6 from "@/assets/layers/6.jpg"
 import logo from "@/assets/logo.jpg"
 import xbrush from "@/assets/xbrushed.png"
 import xbrush2 from "@/assets/xbrushed2.png"
 
 interface ParallaxHeaderProps {
-  children: ReactNode;
+  disableParallax?: boolean,
+  children: ReactNode,
 }
 
 interface ParallaxLayer {
   source: string,
   factor: number,
   isHero?: boolean,
+  isFire?: boolean,
 }
 
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
@@ -30,6 +34,7 @@ const layers: ParallaxLayer[] = [
   { source: layer5, factor: 98 },
   { source: layer4, factor: 95 },
   { source: layer3, factor: 70 },
+  { source: fire, factor: 50, isFire: true },
   { source: layer2, factor: 50 },
   { source: xbrush, factor: 80, isHero: true },
   { source: xbrush2, factor: 90, isHero: true },
@@ -37,7 +42,24 @@ const layers: ParallaxLayer[] = [
   { source: layer1, factor: 0 },
 ];
 
-export default function ParallaxHeader({ children }: ParallaxHeaderProps) {
+export default function ParallaxHeader({ disableParallax, children }: ParallaxHeaderProps) {
+
+  // Static header
+  if (disableParallax) {
+    return (
+      <>
+        <div className="header-container">
+          <img src={full} className="header-layer"/>
+          {layers.filter(d => d.isHero).map((data) => (
+            <img src={data.source} className="header-layer header-hero"/>
+          ))}
+        </div>
+        {children}
+      </>
+    );
+  }
+
+  // Parallax header
   const parallaxRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
@@ -49,7 +71,7 @@ export default function ParallaxHeader({ children }: ParallaxHeaderProps) {
   useEffect(() => {
     if (!parallaxRef.current) return;
 
-    const layersEl = parallaxRef.current.querySelectorAll<HTMLImageElement>('.layer');
+    const layersEl = parallaxRef.current.querySelectorAll<HTMLElement>('.header-layer');
 
     layersEl.forEach((layer, i) => {
       gsap.to(layer, {
@@ -68,12 +90,20 @@ export default function ParallaxHeader({ children }: ParallaxHeaderProps) {
   return (
     <div id="smooth-wrapper">
       <div id="smooth-content">
-        <div className="parallax-container" ref={parallaxRef}>
+        <div className="header-container" ref={parallaxRef}>
           {layers.map((data, index) => (
+            data.isFire ?
+            <div key={index} className="header-layer">
+              <img
+                src={data.source}
+                className="header-fire"
+              />
+            </div>
+            :
             <img
               key={index}
               src={data.source}
-              className={`layer layer-${index + 1}${data.isHero ? ' layer-hero' : ''}`}
+              className={`header-layer ${data.isHero ? ' header-hero' : ''}`}
             />
           ))}
         </div>
